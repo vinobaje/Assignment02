5741f3ee53059bdf87a03c2a67699ff2aa5e1ac5
vinobaje <mvinobaje@gmail.com>
2015-02-07 17:20:35 -0500

    constructor created

--- Assignment02/SuperHero.cs
+++ Assignment02/SuperHero.cs
@@ -6,12 +6,20 @@ namespace Assignment02
 {
     class SuperHero : Hero
     {
+        string[] superPowers;
+        public SuperHero(string name): base(name)
+        {
+            generateAbilities();
+        }
         private void generateAbilities()
         {
             //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
             string[] Powers = { "Super Speed", "Super Strength", "Body Armour", "Flight", "Fire Generation", "Weather Control" };
             string PowerValue;
             //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+            Random rnd = new Random();
+            superPowers = new string[3];
 
         }
     }
